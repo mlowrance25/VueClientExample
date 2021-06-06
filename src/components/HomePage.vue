7efc41af70022e2ee60193545060c99f95a20540
Marvin <36170014+mlowrance25@users.noreply.github.com>
2021-06-05 19:07:52 -0700

    Url Sorting

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,6 +10,10 @@
          :headers="headers"
          :items="customers"
          :search="search"
+         :sort-by="sortBy"
+         :sort-desc="sortDesc"
+         @update:sort-by="updateSort('by', $event)"
+         @update:sort-desc="updateSort('desc', $event)"
          item-key="FirstName"
          class="elevation-1"
 
@@ -33,9 +37,17 @@ import appService from '../app.service.js'
 export default {
     mounted(){
         this.getCustomer();
+         if ('sortBy' in this.$route.query) {
+            this.sortBy = this.$route.query.sortBy;
+        }
+          if ('desc' in this.$route.query) {
+            this.sortDesc = this.$route.query.desc == "true" ? true : false;
+        }
     },
     data() {
         return {
+            sortBy: '',
+            sortDesc: '',
             search : '',
             customers: [
             ],
@@ -52,7 +64,17 @@ export default {
            appService.getCustomers().then(response =>{
                this.customers = response.data;
            })
-        }
+        },
+        updateSort(eventName, event){
+            if(eventName == 'by' && event.length > 0){
+                this.sortBy = event;
+            }
+            if(eventName == 'desc'){
+                 this.sortDesc = event;
+            }
+            this.$router.push({ query: Object.assign({}, this.$route.query, { sortBy: this.sortBy,desc: this.sortDesc }) });
+          
+        },
     }
 }
 </script>
